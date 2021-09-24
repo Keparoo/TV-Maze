c9abd39e4398ee56b3975ac1b5778119820fafdd
Kep <KepCreations@gmail.com>
2021-09-23 21:30:30 -0400

    Implement search Shows

--- tvmaze.js
+++ tvmaze.js
@@ -21,14 +21,18 @@ async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+    res = await axios.get('http://api.tvmaze.com/search/shows', {params: {q :query}})
+
+    return res.data
+
+//   return [
+//     {
+//       id: 1767,
+//       name: "The Bletchley Circle",
+//       summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
+//       image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+//     }
+//   ]
 }
 
 
@@ -41,13 +45,24 @@ function populateShows(shows) {
   const $showsList = $("#shows-list");
   $showsList.empty();
 
+  
+  let image;
   for (let show of shows) {
+
+    // Handle no image returned
+    try {
+        image = show.show.image.medium
+    } catch {
+        image = 'https://tinyurl.com/tv-missing'
+    }
+    
     let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.show.id}">
+         <div class="card" data-show-id="${show.show.id}">
            <div class="card-body">
-             <h5 class="card-title">${show.name}</h5>
-             <p class="card-text">${show.summary}</p>
+           v<img class="card-img-top" src="${image}">
+             <h5 class="card-title">${show.show.name}</h5>
+             <p class="card-text">${show.show.summary}</p>
            </div>
          </div>
        </div>
@@ -87,4 +102,8 @@ async function getEpisodes(id) {
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
   // TODO: return array-of-episode-info, as described in docstring above
+
+  res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`)
+  console.log(res)
+  return res.data
 }
